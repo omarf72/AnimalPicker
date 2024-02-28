5060153dd645b346f820685d5479ee06a3cbfbd8
github user name <github email>
2024-02-28 13:02:07 -0500

    Invalid put and recursive call

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void drawArt()
+    public static void drawArt() throws Exception
         {
             Scanner scan= new Scanner(System.in);
             System.out.println("Hi would you like to see a dog or cat");
@@ -11,6 +11,17 @@ public class App {
             if (answer.equals("dog")) {
                 System.out.println("Dog ACSII Art Here");
             }
+
+            else if(answer.equals("cat")){
+                System.out.println("Cat ACSII Art Here");
+            }
+
+            else {
+               System.out.println("Invalid Input");
+                drawArt(); 
+            }
+
+            
 
         }
     public static void main(String[] args) throws Exception {
