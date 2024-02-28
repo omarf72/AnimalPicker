96542d56419b94175d6a55d308707d16e435d58a
github user name <github email>
2024-02-28 12:30:37 -0500

    Added dog and cat acsii placeholders

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class App {
+    public static void drawArt()
+        {
+            Scanner scan= new Scanner(System.in);
+            System.out.println("Hi would you like to see a dog or cat");
+            String answer=scan.next();
+
+
+            if (answer.equals("dog")) {
+                System.out.println("Dog ACSII Art Here");
+            }
+
+        }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        drawArt();
+        
+
+        
+
+
+
     }
 }
